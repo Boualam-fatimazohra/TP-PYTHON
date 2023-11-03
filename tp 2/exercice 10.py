2a76db48c60472e615c64af28f5725530a85d504
Boualam Fatimazohra <boualamfatima97@gmail.com>
2023-11-03 18:35:29 +0100

    adding

---
+++ tp 2/exercice 10.py
@@ -0,0 +1,10 @@
+L1= [1,3,6,78,35,88,55] 
+L2= [12,24,35,24,88,120,155] 
+#si l'elemente 
+L3=[element for element in L1 if element in L2]
+print(L3)
+#for i in L1:
+    #for j in L2:
+        #if i==j:
+            #L3.append(i)
+#print(L3)
